7b09d363f01fbc0c3baaff8e83df887dd9a092f7
CONURE-SAQEEF\M Saqeef Shabbir <muhammad.shabbir@conurets.com>
2021-09-03 13:19:20 +0500

    Dockerization of react redux application

---
+++ restaurant-application/src/App.js
@@ -0,0 +1,18 @@
+import {BrowserRouter as Router, Switch} from 'react-router-dom';
+
+import Login from './Features/Login/Controller';
+import Main from './Features/Main/Controller';
+import AuthRoute from './Features/Auth/AuthRoute';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <AuthRoute path="/" component={Login} exact={true} type="guest" />
+        <AuthRoute path="/Main" component={Main} type="private" />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
